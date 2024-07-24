bac0ba0f440d2104599c65f4759da35e7fa97037
vtphatt2 <thinhphat544@gmail.com>
2024-07-24 22:16:20 +0700

    add Welcome, Sam

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
@@ -70,6 +71,7 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.material)
     implementation(libs.material3.android)
+    implementation(libs.material3)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -99,4 +101,6 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.6.2")
     implementation("com.squareup.retrofit2:converter-gson:2.1.0")
+
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
 }
--- app/src/main/java/com/example/lemmecook_frontend/activities/ExploreMain.kt
+++ app/src/main/java/com/example/lemmecook_frontend/activities/ExploreMain.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,29 +21,22 @@ class ExploreMain : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LemmeCookFrontendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ExploreMainScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LemmeCookFrontendTheme {
-        Greeting("Android")
-    }
+fun ExploreMainScreen() {
+    Scaffold(
+        content = { innerPadding ->
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)) {
+                Text(text = "Welcome, Sam")
+            }
+        }
+    )
 }
