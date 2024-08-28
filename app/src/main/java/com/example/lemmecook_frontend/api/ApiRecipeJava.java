af13d22d90d07b3842015ec277d5cd6dff0f2118
vtphatt2 <thinhphat544@gmail.com>
2024-08-28 12:30:43 +0700

    get API Recipe for Explore

---
+++ app/src/main/java/com/example/lemmecook_frontend/api/ApiRecipeJava.java
@@ -0,0 +1,18 @@
+package com.example.lemmecook_frontend.api;
+
+import com.example.lemmecook_frontend.models.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiRecipeJava {
+    @GET("recipes/findByIngredients")
+    Call<List<Recipe>> getRecipes(
+            @Query("ingredients") String ingredients,
+            @Query("number") int number,
+            @Query("apiKey") String apiKey
+    );
+}
---
+++ app/src/main/java/com/example/lemmecook_frontend/models/Recipe.java
@@ -0,0 +1,12 @@
+package com.example.lemmecook_frontend.models;
+
+public class Recipe {
+    int id;
+    String title;
+    String image;
+    String imageType;
+
+    public String getTitle() {
+        return title;
+    }
+}
