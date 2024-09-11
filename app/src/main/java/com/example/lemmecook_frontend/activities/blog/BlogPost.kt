952dfd926c6bbb774fd1f6b53b34c64488767336
hhplinh <huynhhaphuonglinh@gmail.com>
2024-09-12 02:09:47 +0700

    Update

--- app/src/main/java/com/example/lemmecook_frontend/activities/blog/BlogPost.kt
+++ app/src/main/java/com/example/lemmecook_frontend/activities/blog/BlogPost.kt
@@ -1,4 +1,10 @@
 package com.example.lemmecook_frontend.activities.blog
 
-class BlogPost {
-}
+data class BlogPost(
+    val title: String,
+    val author: String,
+    val date: Long,
+    val imageUrl: String,
+    val content: String,
+    val link: String
+)
--- app/src/main/java/com/example/lemmecook_frontend/activities/blog/BlogScreen.kt
+++ app/src/main/java/com/example/lemmecook_frontend/activities/blog/BlogScreen.kt
@@ -1,2 +1,54 @@
 package com.example.lemmecook_frontend.activities.blog
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import com.example.lemmecook_frontend.R
+import com.example.lemmecook_frontend.ui.theme.sf_pro_display
+
+@Composable
+fun BlogScreen() {
+    Surface(color = Color.White) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CustomFontText(text = "News Blog", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        }
+
+    }
+}
+
+@Composable
+fun CustomFontText(
+    text: String,
+    fontWeight: FontWeight = FontWeight.SemiBold,
+    color: Color = colorResource(id = R.color.gr_text),
+    fontSize: TextUnit = 16.sp
+) {
+    Text(
+        text = text,
+        fontWeight = fontWeight,
+        fontFamily = sf_pro_display,
+        fontSize = fontSize,
+        color = color
+    )
+}
+
+@Preview
+@Composable
+private fun BlogScreenPreview() {
+    BlogScreen()
+}
