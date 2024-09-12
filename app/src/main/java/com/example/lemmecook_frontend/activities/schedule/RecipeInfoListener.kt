ec878bd5409b0e054dab03a52ae55042cfb70744
hhplinh <huynhhaphuonglinh@gmail.com>
2024-09-12 21:05:07 +0700

    Revert "Update"
    This reverts commit d4a62aed2493de133a5c7e1b75d4eb030ac68d10.

--- app/src/main/java/com/example/lemmecook_frontend/activities/schedule/RecipeInfoListener.kt
+++ app/src/main/java/com/example/lemmecook_frontend/activities/schedule/RecipeInfoListener.kt
@@ -1,7 +1,2 @@
 package com.example.lemmecook_frontend.activities.schedule
 
-import com.example.lemmecook_frontend.models.recipe.RecipeInformation
-
-interface RecipeInfoListener {
-    fun onRecipeInformationUpdated(recipeInformation: RecipeInformation)
-}
