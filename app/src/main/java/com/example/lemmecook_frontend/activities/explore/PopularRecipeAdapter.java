30a92f3276c923022994066b02ba7867805a1e67
vtphatt2 <thinhphat544@gmail.com>
2024-09-14 15:34:02 +0700

    link explore main with recipe overview screen

--- app/src/main/java/com/example/lemmecook_frontend/activities/explore/PopularRecipeAdapter.java
+++ app/src/main/java/com/example/lemmecook_frontend/activities/explore/PopularRecipeAdapter.java
@@ -1,10 +1,12 @@
 package com.example.lemmecook_frontend.activities.explore;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,13 +17,16 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.lemmecook_frontend.R;
+import com.example.lemmecook_frontend.activities.MainActivity;
 import com.example.lemmecook_frontend.models.recipe.Recipe;
+import com.example.lemmecook_frontend.models.viewmodels.RecipeViewModel;
 
 import java.util.List;
 
 public class PopularRecipeAdapter extends RecyclerView.Adapter<PopularRecipeAdapter.ViewHolder>{
     private List<Recipe> recipeList;
     private Context context;
+    private RecipeViewModel recipeViewModel;
 
     public PopularRecipeAdapter(Context context, List<Recipe> recipeList) {
         this.context = context;
@@ -45,6 +50,17 @@ public class PopularRecipeAdapter extends RecyclerView.Adapter<PopularRecipeAdap
                 .load(recipe.getImage()) // Replace with your image URL or resource
                 .apply(requestOptions)
                 .into(holder.imageView);
+
+        holder.ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Recipe recipeChosenByUser = recipeList.get(position);
+                Intent intent = new Intent(context, MainActivity.class);
+                intent.putExtra("startDestination", "RecipeOverviewScreen");
+                intent.putExtra("recipeID", recipeChosenByUser.getId());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -55,11 +71,13 @@ public class PopularRecipeAdapter extends RecyclerView.Adapter<PopularRecipeAdap
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
         TextView textView;
+        LinearLayout ll;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.item_image);
             textView = itemView.findViewById(R.id.item_text);
+            ll = itemView.findViewById(R.id.ll);
         }
     }
 }
